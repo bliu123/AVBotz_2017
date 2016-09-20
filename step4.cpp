c9ea1dfcf6efd913a85a5a1fcdaedb8a11fed262
Benjamin Liu <bliu314159@gmail.com>
2016-09-19 20:36:36 -0700

    Finished.

--- step4.cpp
+++ step4.cpp
@@ -8,16 +8,15 @@ using namespace std;
 
 int main(int arc, char** argv)
 {
-    //Only works if the points are in a plane with a 1 meter grid length.
-    double time1, time2, time3, x1, x2, x3, y1, y2, y3, distance1, distance2, source_x, source_y, equation1_x, equation1_y, equation2_x, equation2_y, least_x, least_y;
+    //Only works if the points are in a plane with a 1 meter grid length and if the sound originat
+    double time1, time2, time3, x1, x2, x3, y1, y2, y3, distance1, distance2, source_x, source_y, equation1_x, equation1_y, equation2_x, equation2_y, least_x, least_y, accuracy_a, accuracy_b, accuracy_c = 1000000000, real_source_x, real_source_y;
     int close;
-    cout << "Input time differences: \n";
+    cout << "Input time differences for signals recieved at A, B, and C in seconds (the input for the first station to recieve a signal should be 0, and the inputs for the other two should be relative to the first): \n";
     cin >> time1 >> time2 >> time3;
-    cout << "Input x-coordinates: \n";
+    cout << "Input x-coordinates for A, B, and C: \n";
     cin >> x1 >> x2 >> x3;
-    cout << "Input y-coordinates: \n";
+    cout << "Input y-coordinates for A, B, and C: \n";
     cin >> y1 >> y2 >> y3;
-    
     
     if (time1 < time2 && time1 < time3)
     {
@@ -102,12 +101,21 @@ int main(int arc, char** argv)
     distance1 *= 1481;
     distance2 *= 1481;
     
-    for (source_x = -1; source_x <= 1; source_x += .01)
+    for (source_x = -100; source_x <= 100; source_x += .01)
         {
-        for (source_y = -1; source_y <= 1; source_y += .01)
+        for (source_y = -100; source_y <= 100; source_y += .01)
             {
-                //Find the most accurate x and y values.
+                accuracy_a = fabs((sqrt(((source_x - equation1_x)*(source_x - equation1_x)) + ((source_y - equation1_y)*(source_y - equation1_y))) - sqrt(((source_x - least_x)*(source_x - least_x)) + ((source_y - least_y)*(source_y - least_y))) - (distance1)));
+                accuracy_b = fabs((sqrt(((source_x - equation2_x)*(source_x - equation2_x)) + ((source_y - equation2_y)*(source_y - equation2_y))) - sqrt(((source_x - least_x)*(source_x - least_x)) + ((source_y - least_y)*(source_y - least_y))) - (distance2)));
+                
+                if ((accuracy_a + accuracy_b) < accuracy_c)
+                {
+                    accuracy_c = (accuracy_a + accuracy_b);
+                    real_source_x = source_x;
+                    real_source_y = source_y;
+                }
+                
             }
         }
-    cout << "(" << source_x << "," << source_y << ")";
+    cout << "Sound originates from: (" << real_source_x << "," << real_source_y << ")\n";
 }
